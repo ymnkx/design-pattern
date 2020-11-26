a3a0a5e5450bfcd00c7bbceeb934f1e512a913b0
Yamanaka Tomohiro <yamanaka@un-t.com>
2020-11-26 18:07:44 +0900

    Update Prototype Pattern

--- src/assets/js/Prototype/Client.ts
+++ src/assets/js/Prototype/Client.ts
@@ -1,15 +1,13 @@
-import Prototype from "./Prototype";
+import Prototype from './Prototype';
 
 export default class Client {
-  // private showcase = new Map<string, Prototype>();
-  // public register(name:string, proto:Prototype) {
-  //   this.showcase.set(name, proto);
-  // }
-  private showcase: { [key: string]: Prototype; } = {};
-  public register(name:string, proto:Prototype) {
+  private showcase: { [key: string]: Prototype } = {};
+
+  public register(name: string, proto: Prototype) {
     this.showcase[name] = proto;
   }
-  public create(name:string): Prototype {
+
+  public create(name: string): Prototype {
     const p = this.showcase[name];
     return p.createClose();
   }
